abb3a18e819e1f9235760c4d54ed0e52be1facff
imhcyx <git@iwannaknow.me>
2021-09-26 20:41:31 +0800

    transform: add a workaround to include uninstalled header files

--- common.mk
+++ common.mk
@@ -1,4 +1,5 @@
 ROOT_DIR := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
+YOSYS_DIR := $(ROOT_DIR)/yosys
 
 TRANSFORM_LIB := $(ROOT_DIR)/transform/transform.so
 TRANSFORM_TCL := $(ROOT_DIR)/scripts/transform.tcl
