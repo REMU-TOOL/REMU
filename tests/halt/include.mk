26ec1b011c7a7b3471f3fa789f0ff22a2d731f81
imhcyx <git@iwannaknow.me>
2021-10-14 16:16:02 +0800

    tests/halt: add srsw_raddr test

--- tests/halt/include.mk
+++ tests/halt/include.mk
@@ -1,4 +1,4 @@
-TESTS-halt := ff srsw_rdata
+TESTS-halt := ff srsw_rdata srsw_raddr
 
 VSRC-halt-ff := ffhalt.v
 VTOP-halt-ff := ffhalt
@@ -7,3 +7,7 @@ VSIM-halt-ff := ffhalt.v ffhalttest.v
 VSRC-halt-srsw_rdata := srsw_rdata.v
 VTOP-halt-srsw_rdata := srsw_rdata
 VSIM-halt-srsw_rdata := srsw_rdata.v srsw_rdata_test.v
+
+VSRC-halt-srsw_raddr := srsw_raddr.v
+VTOP-halt-srsw_raddr := srsw_raddr
+VSIM-halt-srsw_raddr := srsw_raddr.v srsw_raddr_test.v
---
+++ tests/halt/srsw_raddr_test.v
@@ -0,0 +1,67 @@
+`include "test.vh"
+
+module sim_top();
+
+    reg clk = 0;
+    reg halt = 0;
+    reg wen = 0, ren = 0;
+    reg [1:0] waddr = 0, raddr = 0;
+    reg [31:0] wdata = 0;
+    wire [31:0] rdata_dut, rdata_ref;
+
+    \$EMU_DUT emu_dut(
+        .\$EMU$CLK      (clk),
+        .\$EMU$HALT     (halt),
+        .\$EMU$FF$SCAN  (1'd0),
+        .\$EMU$FF$SDI   (64'd0),
+        .\$EMU$FF$SDO   (),
+        .\$EMU$RAM$SCAN (1'd0),
+        .\$EMU$RAM$DIR  (1'd0),
+        .\$EMU$RAM$SDI  (64'd0),
+        .\$EMU$RAM$SDO  (),
+        .wen(wen),
+        .waddr(waddr),
+        .wdata(wdata),
+        .ren(ren),
+        .raddr(raddr),
+        .rdata(rdata_dut)
+    );
+
+    srsw_raddr ref(
+        .clk(clk & !halt),
+        .wen(wen),
+        .waddr(waddr),
+        .wdata(wdata),
+        .ren(ren),
+        .raddr(raddr),
+        .rdata(rdata_ref)
+    );
+
+    always #5 clk = ~clk;
+    always #10 begin
+        halt = $random;
+        wen = $random;
+        waddr = $random;
+        wdata = $random;
+        ren = $random;
+        raddr = $random;
+    end
+
+    always #10 begin
+        $display("%dns: halt=%h wen=%h waddr=%h wdata=%h ren=%h raddr=%h rdata_dut=%h rdata_ref=%h",
+            $time, halt, wen, waddr, wdata, ren, raddr, rdata_dut, rdata_ref);
+        if (rdata_dut !== rdata_ref) begin
+            $display("ERROR: data mismatch");
+            $fatal;
+        end
+    end
+
+    initial begin
+        #5000;
+        $display("success");
+        $finish;
+    end
+
+    `DUMP_VCD
+
+endmodule
