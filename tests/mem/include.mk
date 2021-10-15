c6036eb2b9a4b65a189c27fc3a25650350a54cbd
imhcyx <git@iwannaknow.me>
2021-10-15 10:02:26 +0800

    tests/mem: add offset test

--- tests/mem/include.mk
+++ tests/mem/include.mk
@@ -1,5 +1,9 @@
-TESTS-mem := arsw
+TESTS-mem := arsw offset
 
 VSRC-mem-arsw := arsw.v mem.v
 VTOP-mem-arsw := arsw
 VSIM-mem-arsw := arswtest.v
+
+VSRC-mem-offset := offset.v mem.v
+VTOP-mem-offset := offset
+VSIM-mem-offset := offsettest.v
---
+++ tests/mem/offset.v
@@ -0,0 +1,28 @@
+module offset(
+    input                   clk,
+    input                   rst,
+    input   [5:0]           raddr,
+    output  [79:0]          rdata,
+    input                   wen,
+    input   [5:0]           waddr,
+    input   [79:0]          wdata
+);
+
+    mem #(
+        .WIDTH(80),
+        .DEPTH(32),
+        .OFFSET(32),
+        .SYNCREAD(0)
+    )
+    u_mem (
+        .clk(clk),
+        .rst(rst),
+        .ren(1'b0),
+        .raddr(raddr),
+        .rdata(rdata),
+        .wen(wen),
+        .waddr(waddr),
+        .wdata(wdata)
+    );
+
+endmodule
