82b087900201361f1d01fa4c992fcc9fa34b8192
imhcyx <git@iwannaknow.me>
2022-01-05 09:13:01 +0800

    picorv32: Move rammodel & putchar to uncore module

--- design/picorv32/emu_top.v
+++ design/picorv32/emu_top.v
@@ -1,4 +1,5 @@
 `timescale 1 ns / 1 ps
+`default_nettype none
 
 module emu_top();
 
@@ -43,6 +44,34 @@ module emu_top();
 		.mem_rdata(mem_rdata)
 	);
 
+    emu_uncore uncore (
+		.clk            (clk            ),
+		.resetn         (!rst           ),
+		.mem_valid      (mem_valid      ),
+		.mem_instr      (mem_instr      ),
+		.mem_ready      (mem_ready      ),
+		.mem_addr       (mem_addr       ),
+		.mem_wdata      (mem_wdata      ),
+		.mem_wstrb      (mem_wstrb      ),
+		.mem_rdata      (mem_rdata      )
+    );
+
+endmodule
+
+module emu_uncore (
+	input clk, resetn,
+
+	// Native PicoRV32 memory interface
+
+	input               mem_valid,
+	input               mem_instr,
+	output              mem_ready,
+	input       [31:0]  mem_addr,
+	input       [31:0]  mem_wdata,
+	input       [ 3:0]  mem_wstrb,
+	output      [31:0]  mem_rdata
+);
+
 	wire          mem_axi_awvalid;
 	wire          mem_axi_awready;
 	wire   [31:0] mem_axi_awaddr;
@@ -64,130 +93,6 @@ module emu_top();
 	wire          mem_axi_rvalid;
 	wire          mem_axi_rready;
 	wire   [31:0] mem_axi_rdata;
-
-    picorv32_emu_bus_adapter u_adapter (
-		.clk            (clk            ),
-		.resetn         (!rst           ),
-		.mem_axi_awvalid(mem_axi_awvalid),
-		.mem_axi_awready(mem_axi_awready),
-		.mem_axi_awaddr (mem_axi_awaddr ),
-		.mem_axi_awprot (mem_axi_awprot ),
-		.mem_axi_wvalid (mem_axi_wvalid ),
-		.mem_axi_wready (mem_axi_wready ),
-		.mem_axi_wdata  (mem_axi_wdata  ),
-		.mem_axi_wstrb  (mem_axi_wstrb  ),
-		.mem_axi_bvalid (mem_axi_bvalid ),
-		.mem_axi_bready (mem_axi_bready ),
-		.mem_axi_arvalid(mem_axi_arvalid),
-		.mem_axi_arready(mem_axi_arready),
-		.mem_axi_araddr (mem_axi_araddr ),
-		.mem_axi_arprot (mem_axi_arprot ),
-		.mem_axi_rvalid (mem_axi_rvalid ),
-		.mem_axi_rready (mem_axi_rready ),
-		.mem_axi_rdata  (mem_axi_rdata  ),
-		.mem_valid      (mem_valid      ),
-		.mem_instr      (mem_instr      ),
-		.mem_ready      (mem_ready      ),
-		.mem_addr       (mem_addr       ),
-		.mem_wdata      (mem_wdata      ),
-		.mem_wstrb      (mem_wstrb      ),
-		.mem_rdata      (mem_rdata      )
-    );
-
-    RAMModel #(
-        .ADDR_WIDTH     (32),
-        .DATA_WIDTH     (32),
-        .ID_WIDTH       (1),
-        .R_DELAY        (0),
-        .W_DELAY        (0)
-    )
-    u_rammodel (
-        .aclk           (clk),
-        .aresetn        (!rst),
-        .s_axi_awvalid  (mem_axi_awvalid),
-        .s_axi_awready  (mem_axi_awready),
-        .s_axi_awaddr   (mem_axi_awaddr),
-        .s_axi_awid     (1'b0),
-        .s_axi_awlen    (8'd0),
-        .s_axi_awsize   (3'd2),
-        .s_axi_awburst  (2'd1),
-        .s_axi_awlock   (1'd0),
-        .s_axi_awcache  (4'd0),
-        .s_axi_awprot   (mem_axi_awprot),
-        .s_axi_awqos    (4'd0),
-        .s_axi_awregion (4'd0),
-
-        .s_axi_wvalid   (mem_axi_wvalid),
-        .s_axi_wready   (mem_axi_wready),
-        .s_axi_wdata    (mem_axi_wdata),
-        .s_axi_wstrb    (mem_axi_wstrb),
-        .s_axi_wlast    (1'b1),
-
-        .s_axi_bvalid   (mem_axi_bvalid),
-        .s_axi_bready   (mem_axi_bready),
-        .s_axi_bresp    (),
-        .s_axi_bid      (),
-
-        .s_axi_arvalid  (mem_axi_arvalid),
-        .s_axi_arready  (mem_axi_arready),
-        .s_axi_araddr   (mem_axi_araddr),
-        .s_axi_arid     (1'b0),
-        .s_axi_arlen    (8'd0),
-        .s_axi_arsize   (3'd2),
-        .s_axi_arburst  (2'd1),
-        .s_axi_arlock   (1'd0),
-        .s_axi_arcache  (4'd0),
-        .s_axi_arprot   (mem_axi_arprot),
-        .s_axi_arqos    (4'd0),
-        .s_axi_arregion (4'd0),
-
-        .s_axi_rvalid   (mem_axi_rvalid),
-        .s_axi_rready   (mem_axi_rready),
-        .s_axi_rdata    (mem_axi_rdata),
-        .s_axi_rresp    (),
-        .s_axi_rid      (),
-        .s_axi_rlast    ()
-    );
-
-endmodule
-
-module picorv32_emu_bus_adapter (
-	input clk, resetn,
-
-	// AXI4-lite master memory interface
-
-	output              mem_axi_awvalid,
-	input               mem_axi_awready,
-	output      [31:0]  mem_axi_awaddr,
-	output      [ 2:0]  mem_axi_awprot,
-
-	output              mem_axi_wvalid,
-	input               mem_axi_wready,
-	output      [31:0]  mem_axi_wdata,
-	output      [ 3:0]  mem_axi_wstrb,
-
-	input               mem_axi_bvalid,
-	output              mem_axi_bready,
-
-	output              mem_axi_arvalid,
-	input               mem_axi_arready,
-	output      [31:0]  mem_axi_araddr,
-	output      [ 2:0]  mem_axi_arprot,
-
-	input               mem_axi_rvalid,
-	output              mem_axi_rready,
-	input       [31:0]  mem_axi_rdata,
-
-	// Native PicoRV32 memory interface
-
-	input               mem_valid,
-	input               mem_instr,
-	output              mem_ready,
-	input       [31:0]  mem_addr,
-	input       [31:0]  mem_wdata,
-	input       [ 3:0]  mem_wstrb,
-	output      [31:0]  mem_rdata
-);
 
 	wire aw_fire    = mem_axi_awvalid && mem_axi_awready;
 	wire w_fire     = mem_axi_wvalid && mem_axi_wready;
@@ -255,5 +160,60 @@ module picorv32_emu_bus_adapter (
 
     assign mem_ready        = b_fire || r_fire || uart_ack;
     assign mem_rdata        = mem_axi_rdata;
+
+    RAMModel #(
+        .ADDR_WIDTH     (32),
+        .DATA_WIDTH     (32),
+        .ID_WIDTH       (1),
+        .R_DELAY        (0),
+        .W_DELAY        (0)
+    )
+    u_rammodel (
+        .aclk           (clk),
+        .aresetn        (resetn),
+        .s_axi_awvalid  (mem_axi_awvalid),
+        .s_axi_awready  (mem_axi_awready),
+        .s_axi_awaddr   (mem_axi_awaddr),
+        .s_axi_awid     (1'b0),
+        .s_axi_awlen    (8'd0),
+        .s_axi_awsize   (3'd2),
+        .s_axi_awburst  (2'd1),
+        .s_axi_awlock   (1'd0),
+        .s_axi_awcache  (4'd0),
+        .s_axi_awprot   (mem_axi_awprot),
+        .s_axi_awqos    (4'd0),
+        .s_axi_awregion (4'd0),
+
+        .s_axi_wvalid   (mem_axi_wvalid),
+        .s_axi_wready   (mem_axi_wready),
+        .s_axi_wdata    (mem_axi_wdata),
+        .s_axi_wstrb    (mem_axi_wstrb),
+        .s_axi_wlast    (1'b1),
+
+        .s_axi_bvalid   (mem_axi_bvalid),
+        .s_axi_bready   (mem_axi_bready),
+        .s_axi_bresp    (),
+        .s_axi_bid      (),
+
+        .s_axi_arvalid  (mem_axi_arvalid),
+        .s_axi_arready  (mem_axi_arready),
+        .s_axi_araddr   (mem_axi_araddr),
+        .s_axi_arid     (1'b0),
+        .s_axi_arlen    (8'd0),
+        .s_axi_arsize   (3'd2),
+        .s_axi_arburst  (2'd1),
+        .s_axi_arlock   (1'd0),
+        .s_axi_arcache  (4'd0),
+        .s_axi_arprot   (mem_axi_arprot),
+        .s_axi_arqos    (4'd0),
+        .s_axi_arregion (4'd0),
+
+        .s_axi_rvalid   (mem_axi_rvalid),
+        .s_axi_rready   (mem_axi_rready),
+        .s_axi_rdata    (mem_axi_rdata),
+        .s_axi_rresp    (),
+        .s_axi_rid      (),
+        .s_axi_rlast    ()
+    );
 
 endmodule
