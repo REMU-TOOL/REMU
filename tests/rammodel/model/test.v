287538314162886f6097a7ee1c226a62e58fde1e
imhcyx <git@iwannaknow.me>
2022-03-09 14:18:48 +0800

    tests: Modify rammodel/model

--- tests/rammodel/model/test.v
+++ tests/rammodel/model/test.v
@@ -3,59 +3,65 @@
 `include "axi.vh"
 
 module rammodel_test #(
-    parameter   ADDR_WIDTH              = 32,
-    parameter   DATA_WIDTH              = 64,
-    parameter   ID_WIDTH                = 4
+    parameter   ADDR_WIDTH      = 32,
+    parameter   DATA_WIDTH      = 64,
+    parameter   ID_WIDTH        = 4,
+    parameter   PF_COUNT        = 'h1
 )(
-    input                       clk,
-    input                       resetn,
+    input                       host_clk,
+    input                       host_rst,
 
-    input                       dut_resetn,
+    output                      target_clk,
+    input                       target_rst,
 
-    `AXI4_SLAVE_IF              (s_dut,     ADDR_WIDTH, DATA_WIDTH, ID_WIDTH),
-    `AXI4_MASTER_IF             (m_dram,    ADDR_WIDTH, DATA_WIDTH, ID_WIDTH),
+    `AXI4_SLAVE_IF              (s_axi,     ADDR_WIDTH, DATA_WIDTH, ID_WIDTH),
+    `AXI4_MASTER_IF             (host_axi,  ADDR_WIDTH, DATA_WIDTH, ID_WIDTH),
+    `AXI4_MASTER_IF             (lsu_axi, 32, 32, 1),
 
     input                       pause,
 
     input                       up_req,
     input                       down_req,
     output                      up,
-    output                      down,
-
-    output                      dut_stall,
-
-    // for testbench use
-    output                      dut_clk
+    output                      down
 );
 
-    rammodel_simple #(
-        .ADDR_WIDTH (ADDR_WIDTH),
-        .DATA_WIDTH (DATA_WIDTH),
-        .ID_WIDTH   (ID_WIDTH)
-    )
-    uut (
-        .clk            (clk),
-        .resetn         (resetn && dut_resetn),
-        `AXI4_CONNECT   (s_dut, s_dut),
-        `AXI4_CONNECT   (m_dram, m_dram),
-        .stall          (pause || dut_stall),
-        .up_req         (up_req),
-        .down_req       (down_req),
-        .up             (up),
-        .down           (down),
-        .stall_gen      (dut_stall)
-    );
-
-    wire dut_clk_en = !pause && !dut_stall;
+    wire stall;
+    wire target_fire = !pause && !stall;
 
     reg en_latch;
-    always @(clk or dut_clk_en)
-        if (~clk)
-            en_latch = dut_clk_en;
-    assign dut_clk = clk & en_latch;
+    always @(host_clk or target_fire)
+        if (~host_clk)
+            en_latch = target_fire;
+    assign target_clk = host_clk & en_latch;
 
-    integer dut_cnt = 0;
-    always @(posedge dut_clk) dut_cnt = dut_cnt + 1;
+    integer target_cnt = 0;
+    always @(posedge target_clk) target_cnt <= target_cnt + 1;
+
+    EmuRam #(
+        .ADDR_WIDTH (ADDR_WIDTH),
+        .DATA_WIDTH (DATA_WIDTH),
+        .ID_WIDTH   (ID_WIDTH),
+        .PF_COUNT   (PF_COUNT)
+    )
+    uut (
+        .clk            (target_clk),
+        .rst            (target_rst),
+        `AXI4_CONNECT   (s_axi, s_axi),
+        .host_clk       (host_clk),
+        .host_rst       (host_rst),
+        `AXI4_CONNECT   (host_axi, host_axi),
+        `AXI4_CONNECT_NO_ID (lsu_axi, lsu_axi),
+        .target_fire    (target_fire),
+        .stall          (stall),
+        .up_req         (up_req),
+        .down_req       (down_req),
+        .up_stat        (up),
+        .down_stat      (down)
+    );
+
+    assign lsu_axi_arid = 0;
+    assign lsu_axi_awid = 0;
 
     initial begin
         if ($test$plusargs("DUMP")) begin
--- tests/rammodel/model/test_model.py
+++ tests/rammodel/model/test_model.py
@@ -10,28 +10,29 @@ from cocotbext.axi.constants import AxiBurstType
 class TB:
     def __init__(self, dut):
         self.dut = dut
-        cocotb.fork(Clock(dut.clk, 10, units='ns').start())
-        self.axi_master = AxiMaster(AxiBus.from_prefix(dut, 's_dut'), dut.dut_clk, dut.dut_resetn, reset_active_level=False)
-        self.axi_ram = AxiRam(AxiBus.from_prefix(dut, 'm_dram'), dut.clk, dut.resetn, reset_active_level=False, size=0x1000)
+        cocotb.fork(Clock(dut.host_clk, 10, units='ns').start())
+        self.axi_master = AxiMaster(AxiBus.from_prefix(dut, 's_axi'), dut.target_clk, dut.target_rst)
+        self.axi_ram = AxiRam(AxiBus.from_prefix(dut, 'host_axi'), dut.host_clk, dut.host_rst, size=0x1000)
+        self.axi_lsu_ram = AxiRam(AxiBus.from_prefix(dut, 'lsu_axi'), dut.host_clk, dut.host_rst, size=0x2000)
 
     async def do_reset(self):
-        self.dut._log.info("resetn asserted")
+        self.dut._log.info("reset asserted")
         self.dut.pause.value = 0
         self.dut.up_req.value = 0
         self.dut.down_req.value = 0
-        self.dut.dut_resetn.value = 0
-        self.dut.resetn.value = 0
-        await RisingEdge(self.dut.clk)
-        await RisingEdge(self.dut.clk)
-        self.dut.resetn.value = 1
-        await RisingEdge(self.dut.clk)
-        self.dut._log.info("resetn deasserted")
+        self.dut.target_rst.value = 1
+        self.dut.host_rst.value = 1
+        await RisingEdge(self.dut.host_clk)
+        await RisingEdge(self.dut.host_clk)
+        self.dut.host_rst.value = 0
+        self.dut._log.info("reset deasserted")
+        await RisingEdge(self.dut.host_clk)
 
     async def do_up(self):
         self.dut._log.info("up requested")
         self.dut.up_req.value = 1
         while self.dut.up.value != 1:
-            await RisingEdge(self.dut.clk)
+            await RisingEdge(self.dut.host_clk)
         self.dut.up_req.value = 0
         self.dut._log.info("up acknowledged")
 
@@ -39,7 +40,7 @@ class TB:
         self.dut._log.info("down requested")
         self.dut.down_req.value = 1
         while self.dut.down.value != 1:
-            await RisingEdge(self.dut.clk)
+            await RisingEdge(self.dut.host_clk)
         self.dut.down_req.value = 0
         self.dut._log.info("down acknowledged")
 
@@ -57,34 +58,37 @@ async def run_test(dut):
             length = 0x100 if burst == AxiBurstType.INCR else 16 * (2 ** size)
             test_data = bytearray([x % 256 for x in range(length)])
             dut._log.info("STEP %d: address=0x%x, length=0x%x, axsize=%d axburst=%s" % (step, addr, length, size, burst))
-            await ClockCycles(dut.clk, random.randint(1, 20))
-            await tb.axi_master.write(addr, test_data, burst=burst, size=size)
-            await ClockCycles(dut.clk, random.randint(1, 20))
-            data = await tb.axi_master.read(addr, length, burst=burst, size=size)
+            await ClockCycles(dut.host_clk, random.randint(1, 20))
+            await tb.axi_master.write(addr, test_data, burst=burst, size=size, awid=0)
+            await ClockCycles(dut.host_clk, random.randint(1, 20))
+            data = await tb.axi_master.read(addr, length, burst=burst, size=size, arid=0)
             assert data.data == test_data
 
     running = True
 
     async def pause_resume_worker_func():
         while running:
-            await ClockCycles(dut.clk, random.randint(1, 20))
+            await ClockCycles(dut.host_clk, random.randint(1, 500))
             dut.pause.value = 1
-            await ClockCycles(dut.clk, random.randint(1, 3))
+            await ClockCycles(dut.host_clk, random.randint(1, 3))
             await tb.do_down()
-            await ClockCycles(dut.clk, random.randint(1, 5))
+            await ClockCycles(dut.host_clk, random.randint(1, 5))
             await tb.do_up()
-            await ClockCycles(dut.clk, random.randint(1, 3))
+            await ClockCycles(dut.host_clk, random.randint(1, 3))
             dut.pause.value = 0
+
+    #read_write_worker = cocotb.fork(read_write_worker_func())
+    #pause_resume_worker = cocotb.fork(pause_resume_worker_func())
+
+    await ClockCycles(dut.host_clk, 100)
+    dut._log.info("Releasing DUT reset")
+    dut.target_rst.value = 0
 
     read_write_worker = cocotb.fork(read_write_worker_func())
     pause_resume_worker = cocotb.fork(pause_resume_worker_func())
-
-    await ClockCycles(dut.clk, 100)
-    dut._log.info("Releasing DUT reset")
-    dut.dut_resetn.value = 1
 
     await read_write_worker.join()
     running = False
     await pause_resume_worker.join()
 
-    await RisingEdge(dut.clk)
+    await RisingEdge(dut.host_clk)
