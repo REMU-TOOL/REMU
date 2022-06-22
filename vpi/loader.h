c45c5889f9fe72aec183e8f4adf3da3087b5792b
imhcyx <git@iwannaknow.me>
2022-06-22 16:47:24 +0800

    vpi: rewrite loader to VPILoader & PrintLoader, add scdump utility

--- vpi/loader.h
+++ vpi/loader.h
@@ -6,14 +6,28 @@
 
 namespace Replay {
 
-class Loader {
+class BaseLoader {
     std::string m_sc_file;
     Checkpoint &m_checkpoint;
 
+    virtual void set_ff_value(std::string name, uint64_t data, int width, int offset) = 0;
+    virtual void set_mem_value(std::string name, int index, uint64_t data, int width, int offset) = 0;
+
+    void load_ff(std::istream &data_stream, YAML::Node &config);
+    void load_mem(std::istream &data_stream, YAML::Node &config);
+
 public:
     bool load();
-    Loader(std::string sc_file, Checkpoint &checkpoint)
+    BaseLoader(std::string sc_file, Checkpoint &checkpoint)
         : m_sc_file(sc_file), m_checkpoint(checkpoint) {}
+};
+
+class PrintLoader : public BaseLoader {
+    virtual void set_ff_value(std::string name, uint64_t data, int width, int offset) override;
+    virtual void set_mem_value(std::string name, int index, uint64_t data, int width, int offset) override;
+
+public:
+    PrintLoader(std::string sc_file, Checkpoint &checkpoint) : BaseLoader(sc_file, checkpoint) {}
 };
 
 };
