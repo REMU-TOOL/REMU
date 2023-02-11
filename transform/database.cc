ad150ede3e65bd7221bb8ebad994531533b83b3f
imhcyx <git@iwannaknow.me>
2023-02-11 16:50:49 +0800

    transform: fix ff count in loader

--- transform/database.cc
+++ transform/database.cc
@@ -72,23 +72,23 @@ void EmulationDatabase::write_loader(std::string file_name) {
     os << "`define LOAD_FF(__LOAD_DATA, __LOAD_DUT) \\\n";
     addr = 0;
     for (auto &info : sysinfo.scan_ff) {
-        if (info.name.empty())
-            continue;
-        auto &wire = sysinfo.wire.at(info.name);
-        os << "    __LOAD_DUT";
-        for (auto &name : info.name)
-            os << "." << Escape::escape_verilog_id(name);
-        if (info.width != wire.width) {
-            if (info.width == 1)
-                os << stringf("[%d]", wire.start_offset + info.offset);
-            else if (wire.upto)
-                os << stringf("[%d:%d]", (wire.width - (info.offset + info.width - 1) - 1) + wire.start_offset,
-                        (wire.width - info.offset - 1) + wire.start_offset);
-            else
-                os << stringf("[%d:%d]", wire.start_offset + info.offset + info.width - 1,
-                        wire.start_offset + info.offset);
+        if (!info.name.empty()) {
+            auto &wire = sysinfo.wire.at(info.name);
+            os << "    __LOAD_DUT";
+            for (auto &name : info.name)
+                os << "." << Escape::escape_verilog_id(name);
+            if (info.width != wire.width) {
+                if (info.width == 1)
+                    os << stringf("[%d]", wire.start_offset + info.offset);
+                else if (wire.upto)
+                    os << stringf("[%d:%d]", (wire.width - (info.offset + info.width - 1) - 1) + wire.start_offset,
+                            (wire.width - info.offset - 1) + wire.start_offset);
+                else
+                    os << stringf("[%d:%d]", wire.start_offset + info.offset + info.width - 1,
+                            wire.start_offset + info.offset);
+            }
+            os << " = __LOAD_DATA[" << addr << "+:" << info.width << "]; \\\n";
         }
-        os << " = __LOAD_DATA[" << addr << "+:" << info.width << "]; \\\n";
         addr += info.width;
     }
     os << "\n";
