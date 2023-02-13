945e54bea425f0860db05795c9369406ed4026ae
imhcyx <git@iwannaknow.me>
2023-02-13 20:22:59 +0800

    fastarray: add load & save

--- cosim/fastarray/fastarray_vpi.cc
+++ cosim/fastarray/fastarray_vpi.cc
@@ -1,10 +1,12 @@
 #include "fastarray.h"
 #include "vpi_utils.h"
+#include <fstream>
 
 using namespace REMU;
 
 std::vector<FastArray> fastarray_instances;
 
+// function integer $fastarray_new(input [63:0] width, input [63:0] depth)
 int fastarray_new_calltf(char*)
 {
     vpiHandle callh = vpi_handle(vpiSysTfCall, 0);
@@ -26,6 +28,7 @@ int fastarray_new_calltf(char*)
     return 0;
 }
 
+// function integer $fastarray_read(input integer index, input [63:0] pos, output [width-1:0] data)
 int fastarray_read_calltf(char*)
 {
     vpiHandle callh = vpi_handle(vpiSysTfCall, 0);
@@ -55,6 +58,7 @@ int fastarray_read_calltf(char*)
     return 0;
 }
 
+// function integer $fastarray_write(input integer index, input [63:0] pos, input [width-1:0] data)
 int fastarray_write_calltf(char*)
 {
     vpiHandle callh = vpi_handle(vpiSysTfCall, 0);
@@ -80,6 +84,76 @@ int fastarray_write_calltf(char*)
     }
 
     inst.write(pos, vpiGetValue<BitVector>(args[2]));
+    vpiSetValue(callh, 0);
+    return 0;
+}
+
+// function integer $fastarray_load(input integer index, input string path)
+int fastarray_load_calltf(char*)
+{
+    vpiHandle callh = vpi_handle(vpiSysTfCall, 0);
+    auto args = vpiGetTfArgs(callh);
+
+    if (args.size() != 2) {
+        vpi_printf("%s: wrong number of arguments\n", __func__);
+        vpiSetValue(callh, -1);
+        return 0;
+    }
+
+    auto index = vpiGetValue<int>(args[0]);
+    if (index < 0 || index >= fastarray_instances.size()) {
+        vpiSetValue(callh, -1);
+        return 0;
+    }
+    auto &inst = fastarray_instances.at(index);
+
+    auto path = vpiGetValue<std::string>(args[1]);
+    std::ifstream f(path, std::ios::binary);
+    if (f.fail()) {
+        vpiSetValue(callh, -1);
+        return 0;
+    }
+
+    if (!inst.load(f)) {
+        vpiSetValue(callh, -1);
+        return 0;
+    }
+
+    vpiSetValue(callh, 0);
+    return 0;
+}
+
+// function integer $fastarray_save(input integer index, input string path)
+int fastarray_save_calltf(char*)
+{
+    vpiHandle callh = vpi_handle(vpiSysTfCall, 0);
+    auto args = vpiGetTfArgs(callh);
+
+    if (args.size() != 2) {
+        vpi_printf("%s: wrong number of arguments\n", __func__);
+        vpiSetValue(callh, -1);
+        return 0;
+    }
+
+    auto index = vpiGetValue<int>(args[0]);
+    if (index < 0 || index >= fastarray_instances.size()) {
+        vpiSetValue(callh, -1);
+        return 0;
+    }
+    auto &inst = fastarray_instances.at(index);
+
+    auto path = vpiGetValue<std::string>(args[1]);
+    std::ofstream f(path, std::ios::binary);
+    if (f.fail()) {
+        vpiSetValue(callh, -1);
+        return 0;
+    }
+
+    if (!inst.save(f)) {
+        vpiSetValue(callh, -1);
+        return 0;
+    }
+
     vpiSetValue(callh, 0);
     return 0;
 }
@@ -110,6 +184,24 @@ void fastarray_register()
     tf_data.sysfunctype = vpiIntFunc;
     tf_data.tfname      = "$fastarray_write";
     tf_data.calltf      = fastarray_write_calltf;
+    tf_data.compiletf   = 0;
+    tf_data.sizetf      = 0;
+    tf_data.user_data   = 0;
+    vpi_register_systf(&tf_data);
+
+    tf_data.type        = vpiSysFunc;
+    tf_data.sysfunctype = vpiIntFunc;
+    tf_data.tfname      = "$fastarray_load";
+    tf_data.calltf      = fastarray_load_calltf;
+    tf_data.compiletf   = 0;
+    tf_data.sizetf      = 0;
+    tf_data.user_data   = 0;
+    vpi_register_systf(&tf_data);
+
+    tf_data.type        = vpiSysFunc;
+    tf_data.sysfunctype = vpiIntFunc;
+    tf_data.tfname      = "$fastarray_save";
+    tf_data.calltf      = fastarray_save_calltf;
     tf_data.compiletf   = 0;
     tf_data.sizetf      = 0;
     tf_data.user_data   = 0;
