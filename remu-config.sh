67108fb946150fd5d317458a34394603d9b778e9
imhcyx <git@iwannaknow.me>
2023-03-06 09:51:03 +0800

    remu-config: support FPGA flow includes/sources

--- remu-config.sh
+++ remu-config.sh
@@ -6,6 +6,9 @@ help() {
 Usage: $0 <option>
 
 Options:
+    --plat <plat>       specify platform
+    --includes          verilog include directories for FPGA flow
+    --sources           verilog sources for FPGA flow
     --ivl-srcs          iverilog simulation sources
     --ivl-flags         iverilog flags
     --vvp-flags         vvp flags
@@ -23,66 +26,137 @@ REMU_DIR=$(realpath $SCRIPT_DIR/../share/remu)
 EMULIB_DIR=$REMU_DIR/emulib
 
 if [[ ! -d $REMU_DIR || ! -d $EMULIB_DIR ]]; then
-    echo REMU is not properly installed. Run this script after installation.
+    echo REMU is not properly installed. Run this script after installation. >&2
     exit 1
 fi
 
-IVL_SRCS=()
-IVL_SRCS+=($(find $EMULIB_DIR/sim -name '*.v'))
+get_includes() {
+    INCLUDES=()
+    INCLUDES+=($EMULIB_DIR/include)
+}
 
-IVL_FLAGS=()
-IVL_FLAGS+=(-I $EMULIB_DIR/include)
-IVL_FLAGS+=(-s remu_replay)
-IVL_FLAGS+=(-s EMU_TOP)
+get_sources() {
+    if [ -z $1 ]; then
+        echo Platform must be specified. >&2
+        exit 1
+    fi
 
-VVP_FLAGS=()
-VVP_FLAGS+=(-M $REMU_DIR)
-VVP_FLAGS+=(-m replay_ivl)
+    if [ ! -d $EMULIB_DIR/platform/$1 ]; then
+        echo Unsupported platform $1. >&2
+        exit 1
+    fi
 
-COSIM_IVL_SRCS=()
-COSIM_IVL_SRCS+=($(find $EMULIB_DIR/common -name '*.v'))
-COSIM_IVL_SRCS+=($(find $EMULIB_DIR/system -name '*.v'))
-COSIM_IVL_SRCS+=($(find $EMULIB_DIR/platform/sim -name '*.v'))
-COSIM_IVL_SRCS+=($(find $REMU_DIR/cosim -name '*.v'))
+    SOURCES=()
+    SOURCES+=($(find $EMULIB_DIR/common -name '*.v'))
+    SOURCES+=($(find $EMULIB_DIR/system -name '*.v'))
+    SOURCES+=($(find $EMULIB_DIR/platform/$1 -name '*.v'))
+}
 
-COSIM_IVL_FLAGS=()
-COSIM_IVL_FLAGS+=(-I $EMULIB_DIR/include)
-COSIM_IVL_FLAGS+=(-I $REMU_DIR/cosim/include)
-COSIM_IVL_FLAGS+=(-s sim_top)
+get_ivl_srcs() {
+    IVL_SRCS=()
+    IVL_SRCS+=($(find $EMULIB_DIR/sim -name '*.v'))
+}
 
-COSIM_VVP_FLAGS=()
-COSIM_VVP_FLAGS+=(-M $REMU_DIR)
-COSIM_VVP_FLAGS+=(-m cosim_ivl)
+get_ivl_flags() {
+    IVL_FLAGS=()
+    get_includes
+    for x in "${INCLUDES[@]}"; do
+        IVL_FLAGS+=(-I $x)
+    done
+    IVL_FLAGS+=(-s remu_replay)
+    IVL_FLAGS+=(-s EMU_TOP)
+}
+
+get_vvp_flags() {
+    VVP_FLAGS=()
+    VVP_FLAGS+=(-M $REMU_DIR)
+    VVP_FLAGS+=(-m replay_ivl)
+}
+
+get_cosim_ivl_srcs() {
+    COSIM_IVL_SRCS=()
+    get_sources "sim"
+    for x in "${SOURCES[@]}"; do
+        COSIM_IVL_SRCS+=($x)
+    done
+    COSIM_IVL_SRCS+=($(find $REMU_DIR/cosim -name '*.v'))
+}
+
+get_cosim_ivl_flags() {
+    COSIM_IVL_FLAGS=()
+    get_includes
+    for x in "${INCLUDES[@]}"; do
+        COSIM_IVL_FLAGS+=(-I $x)
+    done
+    COSIM_IVL_FLAGS+=(-I $REMU_DIR/cosim/include)
+    COSIM_IVL_FLAGS+=(-s sim_top)
+}
+
+get_cosim_vvp_flags() {
+    COSIM_VVP_FLAGS=()
+    COSIM_VVP_FLAGS+=(-M $REMU_DIR)
+    COSIM_VVP_FLAGS+=(-m cosim_ivl)
+}
 
 if [ $# -eq 0 ]; then
     help
 fi
 
-for opt; do
-    case "$opt" in
+ACTION=opt
+PLAT=""
+
+for arg; do
+    case "$ACTION" in
+    opt)
+        case "$arg" in
+        --plat)
+            ACTION=plat
+            ;;
+        --includes)
+            get_includes
+            echo "${INCLUDES[@]}"
+            exit
+            ;;
+        --sources)
+            get_sources $PLAT
+            echo "${SOURCES[@]}"
+            exit
+            ;;
         --ivl-srcs)
+            get_ivl_srcs
             echo "${IVL_SRCS[@]}"
             exit
-        ;;
+            ;;
         --ivl-flags)
+            get_ivl_flags
             echo "${IVL_FLAGS[@]}"
             exit
-        ;;
+            ;;
         --vvp-flags)
+            get_vvp_flags
             echo "${VVP_FLAGS[@]}"
             exit
-        ;;
+            ;;
         --cosim-ivl-srcs)
+            get_cosim_ivl_srcs
             echo "${COSIM_IVL_SRCS[@]}"
             exit
-        ;;
+            ;;
         --cosim-ivl-flags)
+            get_cosim_ivl_flags
             echo "${COSIM_IVL_FLAGS[@]}"
             exit
-        ;;
+            ;;
         --cosim-vvp-flags)
+            get_cosim_vvp_flags
             echo "${COSIM_VVP_FLAGS[@]}"
             exit
+            ;;
+        esac
+        ;;
+    plat)
+        PLAT=$arg
+        ACTION=opt
         ;;
     esac
 done
