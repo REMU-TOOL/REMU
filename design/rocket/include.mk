43b6699c4f8fcf809e0e6e3a2afb40a3de02d0b9
imhcyx <git@iwannaknow.me>
2023-03-20 10:53:32 +0800

    design: move workload to a separate directory

--- design/rocket/include.mk
+++ design/rocket/include.mk
@@ -1,3 +1,8 @@
 VTOP := emu_top
 VSRCS += $(wildcard $(DESIGN)/verilog/*.v)
 COSIM_VSRCS += $(wildcard $(DESIGN)/cosim/*.v)
+
+ISA := riscv64
+MEM_BASE := 0x80000000
+MEM_SIZE := 0x10000000
+UART_BASE := 0x60000000
